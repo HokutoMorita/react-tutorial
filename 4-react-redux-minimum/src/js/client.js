de204472c2093ec2ba2378634f3c88852936e9e7
HokutoMorita <han2210mh@gmail.com>
2021-05-02 09:31:03 +0900

    [add]複数のreduxを処理

--- 4-react-redux-minimum/src/js/client.js
+++ 4-react-redux-minimum/src/js/client.js
@@ -1,24 +1,39 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const reducer = (state = 0, action) => {
-    console.log("reducer has been called.");
+const userReducer = (state = {}, action) => {
     switch(action.type) {
-        case "INC":
-            return state + action.payload;
-        case "DEC":
-            return state - action.payload;
+        case "CHANGE_NAME":
+            state = {...state, name: action.payload};
+            break;
+        case "CHANGE_AGE":
+            state = {...state, age: action.payload};
+            break;
     }
     return state;
 }
 
-const store = createStore(reducer, 1);
+const tweetsReducer = (state = [], action) => {
+    switch(action.type) {
+        case "ADD_TWEET":
+            state = state.concat({id: Date.now(), text: action.payload});
+    }
+    return state;
+}
+
+const reducers = combineReducers({
+    user: userReducer,
+    tweets: tweetsReducer
+});
+
+const store = createStore(reducers);
 
 store.subscribe(() => {
     console.log("store changed", store.getState());
 });
 
-store.dispatch({type: "INC", payload: 1});
-store.dispatch({type: "INC", payload: 2});
-store.dispatch({type: "INC", payload: 22});
-store.dispatch({type: "INC", payload: 222});
-store.dispatch({type: "DEC", payload: 1000});
+store.dispatch({type: "FOO", payload: "BAR"});
+store.dispatch({type: "CHANGE_NAME", payload: "morita"});
+store.dispatch({type: "CHANGE_AGE", payload: 23});
+store.dispatch({type: "CHANGE_AGE", payload: 24});
+store.dispatch({type: "ADD_TWEET", payload: "OMG LIKE LOL"});
+store.dispatch({type: "ADD_TWEET", payload: "I am so like seriously like totally like right now"});
